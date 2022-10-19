2491b165fc20457f60946c8f504683d1af13d9cc
bigmosi <kinyeramo@gmail.com>
2022-10-19 17:28:00 +0300

    Fixed api issues

--- db/migrate/20221019083514_create_greatings.rb
+++
@@ -1,9 +0,0 @@
-class CreateGreatings < ActiveRecord::Migration[7.0]
-  def change
-    create_table :greatings do |t|
-      t.string :message
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_083514) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_082128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "greatings", force: :cascade do |t|
-    t.string "message"
+  create_table "messages", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
